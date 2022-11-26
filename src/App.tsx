bad7fa0a966bf50e3df45781b136f488933aaf64
moteloumka <73367393+moteloumka@users.noreply.github.com>
2022-11-26 12:46:08 +0100

    implement button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState(true); 
+  const changeColor = () => {
+    setState(!state);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={changeColor} className={'button ' + (state ? 'creation--green' :'creation--red')}>
+        click to change change color
+      </button>
     </div>
   );
 }
